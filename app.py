85fe34758663aea45f4837e07bdca9408493e94f
Abhi2572 <149356870+Abhi2572@users.noreply.github.com>
2025-10-03 00:40:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pandas import ExcelWriter
 import io
 
-st.set_page_config(page_title="Agentic Report Generator", layout="wide")
+st.set_page_config(page_title="Agentic Report Generator", layout="fit")
 st.title("🤖 Agentic AI – Manufacturing Report Generator")
 
 uploaded_file = st.file_uploader("📤 Upload your manufacturing CSV file", type=["csv"])
@@ -133,3 +133,4 @@ if uploaded_file:
         file_name="agentic_report.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
